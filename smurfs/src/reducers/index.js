0c67e997a623a724972ad23a8ab0d2f2b7f13ad9
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-10-27 18:39:20 -0500

    build reducer for api call

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,4 +1,5 @@
-import { isNull } from "util";
+import { FETCH_SMURF_START, FETCH_SMURF_SUCCESS, FETCH_SMURF_ERROR } from "../actions"
+import { truncate } from "fs";
 
 const initialState = {
   smurf: null,
@@ -7,5 +8,25 @@ const initialState = {
 };
 
 export function reducer(state = initialState, action) {
-  return state 
+  switch (action.type) {
+    case FETCH_SMURF_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case FETCH_SMURF_SUCCESS: 
+    return {
+      ...state, 
+      smurf: action.payload,
+      isLoading: false
+      };
+    case FETCH_SMURF_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    default: 
+      return state;
+  }
 }
